d2b947e77139318859230597db61634cccaaf11d
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-04-11 23:44:25 +0530

    Added paths and fixed styles

--- frontend/src/AccountPage/LoginPage.tsx
+++ frontend/src/AccountPage/LoginPage.tsx
@@ -1,24 +1,34 @@
-import './LoginPage.css';
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
 
 function LoginPage() {
-    return (
-        <div className="acc-page-body">
-            <form className='acc-form'>
-                <h1>Login</h1>
-                <div className="input-fields-container-acc-page">
-                        <label className='form-input'>
-                            <input type="text" name="username" placeholder='Username'/>
-                        </label>
-                        <label className='form-input'>
-                            <input type="password" name="password" placeholder='Password' />
-                        </label>
-                </div>
-                <h4 className='fp'>Forgot Password</h4>
-                <button className='acc-page-submit-btn' type="submit">Submit</button>
-                <h4>Not a Member? <span className='nam-reg'>Register</span></h4>
-            </form>
+  return (
+    <div className="acc-page-body">
+      <form className="acc-form">
+        <h1>Login</h1>
+        <div className="input-fields-container-acc-page">
+          <label className="form-input">
+            <input type="text" name="username" placeholder="Username" />
+          </label>
+          <label className="form-input">
+            <input type="password" name="password" placeholder="Password" />
+          </label>
         </div>
-    );
+        <h4 className="fp">Forgot Password</h4>
+        <button className="acc-page-submit-btn" type="submit">
+          Submit
+        </button>
+        <h4>
+          Not a Member?{" "}
+          <Link 
+          style={{ textDecoration: 'none' }}
+          to={{ pathname: "/register" }}>
+            <span className="nam-reg">Register</span>
+          </Link>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
